8f98405b62c4d3879df7bc9afd6d963677ecf59f
Atang Sutisna <atang@bippo.co.id>
2019-02-18 16:12:11 +0700

    first commit

---
+++ application/libraries/Simple_login.php
@@ -0,0 +1,64 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Simple_login {
+	
+	// SET SUPER GLOBAL
+	var $CI = NULL;
+	public function __construct() {
+		$this->CI =& get_instance();
+	}
+	
+	// Login
+	public function login($username, $password) {
+		// Query untuk pencocokan data
+		$result = $this->CI->db->get_where('users', array(
+										'username' => $username, 
+										'password' => $password
+										))->row();
+										
+		// Jika ada hasilnya
+		if($result != NULL) {
+			//var_dump($result);
+			$id 	= $result->id_user;
+			$nama	= $result->nama;
+			$level	= $result->akses_level;
+			// $_SESSION['username'] = $username;
+			$this->CI->session->set_userdata('username', $username); 
+			$this->CI->session->set_userdata('akses_level', $level); 
+			$this->CI->session->set_userdata('nama', $nama); 
+			$this->CI->session->set_userdata('id_login', uniqid(rand()));
+			$this->CI->session->set_userdata('id', $id);
+			// Kalau benar di redirect
+		
+			echo "benar pak";
+			//redirect(base_url('admin/dasbor'));
+		}else{
+			$this->CI->session->set_flashdata('sukses','Oopss.. Username/password salah');
+			redirect(base_url().'login');
+		}
+		return false;
+	}
+	
+	// Cek login
+	public function cek_login() {
+		if($this->CI->session->userdata('username') == '' && $this->CI->session->userdata('akses_level')=='') {
+			$this->CI->session->set_flashdata('sukses','Oops...silakan login dulu');
+			redirect(base_url('login'));
+		}	
+	}
+	
+	// Logout
+	public function logout() {
+		$this->CI->session->unset_userdata('username');
+		$this->CI->session->unset_userdata('akses_level');
+		$this->CI->session->unset_userdata('nama');
+		$this->CI->session->unset_userdata('id_login');
+		$this->CI->session->unset_userdata('id');
+		unset($_SESSION['admin']);
+		session_destroy();
+		$this->CI->session->set_flashdata('sukses','Terimakasih, Anda berhasil logout');
+		redirect(base_url().'login');
+	}
+	
+}
