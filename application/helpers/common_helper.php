c4c30a2dad78b48721b4a4b250763dd9ce55006f
Atang Sutisna <atang.sutisna.87@gmail.com>
2019-02-24 03:22:18 +0700

    create helper for dropdown choices

--- application/helpers/common_helper.php
+++ application/helpers/common_helper.php
@@ -11,7 +11,7 @@ if (! function_exists('to_dropdown_choices'))
      */
     function to_dropdown_choices($choices, $value, $display_value)
     {
-        $dropdown_choices = [NULL => '--Pilih--'];
+        $dropdown_choices = [NULL => '--All--'];
         foreach ($choices as $choice) {
             $dropdown_choices[$choice->$value] = $choice->$display_value;
         }
