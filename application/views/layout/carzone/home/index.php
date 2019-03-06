b74077ff7505448cd2f9e37e30f7a64c23b76c7a
Atang Sutisna <atang.sutisna.87@gmail.com>
2019-03-06 13:21:59 +0700

    fix for your quicklook

--- application/views/layout/carzone/home/index.php
+++ application/views/layout/carzone/home/index.php
@@ -16,116 +16,88 @@ $background_uri = base_url('assets/images/background/home-bg.jpg');
             <div class="dlab-separator bg-gray-dark"></div>
             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry has been the industry's standard dummy text ever since the been when an unknown printer.</p>
         </div>
-        <!--
+        
+        <!-- Car Find And Sale -->
         <div class="row">
-            <div class="col-md-3 col-sm-6  col-xs-6">
-                <div class="dlab-box-bg m-b30 box-hover" style="background-image: url(images/our-work/work/pic1.jpg)">
-                    <div class="icon-bx-wraper center p-lr20 p-tb30">
-                        <div class="text-primary m-b20"> 
-                            <span class="icon-cell"><i class="glyph-icon flaticon-steering-wheel"></i></span> 
-                        </div>
-                        <div class="icon-content">
-                            <h4 class="dlab-tilte text-uppercase">All brands</h4>
-                            <p>Lorem Ipsum is simply dummy text of the printing and type setting industry.</p>
-                        </div>
-                    </div>
-                    <div class="icon-box-btn text-center">
-                        <a href="#" class="site-button btn-block">Read More</a>
+            <div class="col-md-4 col-sm-4">
+                <div class="find-car box-hover " style="background-image: url(images/blog/default/thum1.jpg)">
+                    <div class="car-box">
+                        <span class="text-primary">Are You Looking</span>
+                        <h2 class="text-uppercase">To Buy A Car ?</h2>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the..</p>
+                        <a href="new-car-search.html" class="site-button outline">
+                            Search Your Car
+                        </a> 
                     </div>
                 </div>
             </div>
-            <div class="col-md-3 col-sm-6 col-xs-6">
-                <div class="dlab-box-bg m-b30 box-hover" style="background-image: url(images/our-work/work/pic2.jpg)">
-                    <div class="icon-bx-wraper center p-lr20 p-tb30">
-                        <div class="text-primary m-b20"> 
-                            <span class="icon-cell"><i class="glyph-icon flaticon-speech-bubble"></i></span> 
-                        </div>
-                        <div class="icon-content">
-                            <h4 class="dlab-tilte text-uppercase">Free Support</h4>
-                            <p>Lorem Ipsum is simply dummy text of the printing and type setting industry.</p>
-                        </div>
-                    </div>
-                    <div class="icon-box-btn text-center">
-                        <a href="#" class="site-button btn-block">Read More</a>
+            <div class="col-md-4 col-sm-4">
+                <div class="find-car box-hover active" style="background-image: url(images/blog/default/thum2.jpg)">
+                    <div class="car-box">
+                        <span class="text-primary">Are You Looking</span>
+                        <h2 class="text-uppercase">To Buy A Car ?</h2>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the..</p>
+                        <a href="new-car-search.html" class="site-button outline">
+                            Search Your Car
+                        </a> 
                     </div>
                 </div>
             </div>
-            <div class="col-md-3 col-sm-6 col-xs-6">
-                <div class="dlab-box-bg m-b30 box-hover active" style="background-image: url(images/our-work/work/pic3.jpg)">
-                    <div class="icon-bx-wraper center p-lr20 p-tb30">
-                        <div class="text-primary m-b20"> 
-                            <span class="icon-cell"><i class="glyph-icon flaticon-mechanic"></i></span> 
-                        </div>
-                        <div class="icon-content">
-                            <h4 class="dlab-tilte text-uppercase">Dealership</h4>
-                            <p>Lorem Ipsum is simply dummy text of the printing and type setting industry.</p>
-                        </div>
-                    </div>
-                    <div class="icon-box-btn text-center">
-                        <a href="#" class="site-button btn-block">Read More</a>
+            <div class="col-md-4 col-sm-4">
+                <div class="find-car box-hover" style="background-image: url(images/blog/default/thum3.jpg)">
+                    <div class="car-box">
+                        <span class="text-primary">Are You Looking</span>
+                        <h2 class="text-uppercase">To Buy A Car ?</h2>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the..</p>
+                        <a href="new-car-search.html" class="site-button outline">
+                            Search Your Car
+                        </a> 
                     </div>
                 </div>
             </div>
-            <div class="col-md-3 col-sm-6 col-xs-6">
-                <div class="dlab-box-bg m-b30 box-hover" style="background-image: url(images/our-work/work/pic4.jpg)">
-                    <div class="icon-bx-wraper center p-lr20 p-tb30">
-                        <div class="text-primary m-b20"> 
-                            <span class="icon-cell"><i class="glyph-icon flaticon-list"></i></span> 
-                        </div>
-                        <div class="icon-content">
-                            <h4 class="dlab-tilte text-uppercase">affordable</h4>
-                            <p>Lorem Ipsum is simply dummy text of the printing and type setting industry.</p>
-                        </div>
-                    </div>
-                    <div class="icon-box-btn text-center">
-                        <a href="#" class="site-button btn-block">Read More</a>
+		</div> <!-- Car Find And Sale END -->        
+    </div> <!--/end container -->
+</div> <!--/ end section about us -->
+<!-- For Your Quick Look -->
+<div class="section-full bg-white content-inner car-listing">
+    <div class="container">
+        <div class="section-head">
+            <h3 class="h3 text-uppercase">For your quick look</h3>
+        </div>
+        <div class="section-content ">
+            <div class="row">	
+                <div class="dlab-tabs">                    
+                    <div class="tab-content">
+                        <div id="upcoming"  class="tab-pane active clearfix">
+                            <div class="col-md-12 owl-carousel owl-btn-style-2 quick-look">
+                                <?php foreach($produk as $produk) : ?>
+                                <div class="item">
+                                    <div class="dlab-feed-list">
+                                        <div class="dlab-media" style="width: 293px; height: 181px;"> 
+                                            <a href="new-car-upcoming.html">
+                                                <img src="<?= base_url('assets/upload/image/'.$produk->gambar) ?>" alt="">
+                                            </a> 
+                                        </div>
+                                        <div class="dlab-info" style="width: 293px; height: 167px;">
+                                            <h4 class="dlab-title">
+                                                <a href="new-car-upcoming.html">
+                                                    <?= $produk->nama_produk ?>
+                                                </a>
+                                            </h4>
+                                            <div class="dlab-separator bg-black"></div>
+                                            <p><span class="text-primary"><?= format_rupiah($produk->harga) ?></span></p>
+                                        </div>
+                                        <div class="icon-box-btn text-center">
+                                        </div>
+                                    </div>
+                                </div>
+                                <?php endforeach; ?>
+                            </div>
+                        </div> <!--/end of uncoming -->
                     </div>
                 </div>
             </div>
-        </div> -->
-<!-- Car Find And Sale -->
-<div class="section-full bg-gray p-tb20 car-listing">
-            <div class="container">
-				<div class="row">
-					<div class="col-md-4 col-sm-4">
-						<div class="find-car box-hover " style="background-image: url(images/blog/default/thum1.jpg)">
-							<div class="car-box">
-								<span class="text-primary">Are You Looking</span>
-								<h2 class="text-uppercase">To Buy A Car ?</h2>
-								<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the..</p>
-								<a href="new-car-search.html" class="site-button outline">
-									Search Your Car
-								</a> 
-							</div>
-						</div>
-					</div>
-					<div class="col-md-4 col-sm-4">
-						<div class="find-car box-hover active" style="background-image: url(images/blog/default/thum2.jpg)">
-							<div class="car-box">
-								<span class="text-primary">Are You Looking</span>
-								<h2 class="text-uppercase">To Buy A Car ?</h2>
-								<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the..</p>
-								<a href="new-car-search.html" class="site-button outline">
-									Search Your Car
-								</a> 
-							</div>
-						</div>
-					</div>
-					<div class="col-md-4 col-sm-4">
-						<div class="find-car box-hover" style="background-image: url(images/blog/default/thum3.jpg)">
-							<div class="car-box">
-								<span class="text-primary">Are You Looking</span>
-								<h2 class="text-uppercase">To Buy A Car ?</h2>
-								<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the..</p>
-								<a href="new-car-search.html" class="site-button outline">
-									Search Your Car
-								</a> 
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-        <!-- Car Find And Sale END -->        
+        </div>
     </div>
 </div>
+<!-- For Your Quick Look END -->
