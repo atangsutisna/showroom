188cafd5c33b9e9c5a7ee7eadffcce207d7b9ff5
Atang Sutisna <atang.sutisna.87@gmail.com>
2019-03-07 21:41:29 +0700

    fixing layout berita

--- application/views/layout/carzone/berita/index.php
+++ application/views/layout/carzone/berita/index.php
@@ -23,9 +23,12 @@ $background_uri = base_url('assets/images/background/bg5.jpg');
 <div class="section-full content-inner bg-white">
     <div class="container">
         <div class="row">
+            <?php 
+            $row = 1;
+            foreach ($posts as $post) : 
+            ?>
             <!-- blog grid -->
             <div id="masonry" class="dlab-blog-grid-2">
-                <?php foreach ($posts as $post) : ?>
                 <div class="post card-container col-lg-6 col-md-6 col-sm-6 col-xs-12">
                     <div class="blog-post blog-grid date-style-2">
                         <div class="dlab-post-media dlab-img-effect zoom-slow"> 
@@ -44,16 +47,21 @@ $background_uri = base_url('assets/images/background/bg5.jpg');
                                 </ul>
                             </div>
                             <div class="dlab-post-text">
-                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy 
-                                    text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                                <?= substr($post->keterangan, 0, 300) ?>
                             </div>
-                            <div class="dlab-post-readmore"> <a href="#" title="READ MORE" rel="bookmark" class="site-button-link">READ MORE<i class="fa fa-angle-double-right"></i></a> </div>
+                            <div class="dlab-post-readmore"> <a href="<?= base_url('berita/read/'.$post->slug_berita) ?>" title="READ MORE" rel="bookmark" class="site-button-link">READ MORE<i class="fa fa-angle-double-right"></i></a> </div>
                             
                         </div>
                     </div>
                 </div>
-                <?php endforeach; ?>
             </div>
-        </div>    
+            <?php 
+            $row++;
+            if ($row > 2) {
+                echo "</div><div class=\"row\">";
+            }
+            endforeach; 
+            ?>
+        </div> <!--/end row -->  
     </div>
 </div>
