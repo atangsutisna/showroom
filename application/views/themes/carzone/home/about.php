3029e98b82f897ef82cd7624a8d499c77de4bc8f
Atang Sutisna <atang.sutisna.87@gmail.com>
2019-03-08 01:06:28 +0700

    update about me

--- application/views/themes/carzone/home/about.php
+++ application/views/themes/carzone/home/about.php
@@ -4,8 +4,8 @@
             <div class="row">
                 <div class="col-md-5">
                     <div class="">
-                        <h3 class="h3 text-uppercase m-b10 m-t0">About us </h3>
-                        <p class="p-t0">Everything you need to build an amazing dealership website.</p>
+                        <h3 class="h3 text-uppercase m-b10 m-t0">Info Pemesanan</h3>
+                        <p>Silahkan menghubungi kami untuk pemesanan atau pembelian mobil Honda Anda. Kami akan segera menghubungi Anda.</p>
                     </div>
                     <div class="about-us-info">
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
@@ -16,7 +16,7 @@
                                 </a>
                             </div>
                             <div class="media-body p-l15">
-                                <h4 class="font-weight-700 text-uppercase text-primary m-b10">Have any question ?</h4>
+                                <h4 class="font-weight-700 text-uppercase text-primary m-b10">Hubungi: </h4>
                                 <h2 class="media-heading open-sans font-weight-700">01 123 456 789</h2>
                             </div>
                         </div>
